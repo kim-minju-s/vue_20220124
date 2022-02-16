9214b91b04d50ba0c82ac806780e9bc453010a57
dlxorb96 <blesty5683@naver.com>
2022-02-16 17:51:52 +0900

    20220216

--- src/components/seller/Menu5.vue
+++ src/components/seller/Menu5.vue
@@ -4,10 +4,8 @@
         <div style="max-width:300px;height:200px;
             display: flex;flex-direction:column;">
             <vue3-chart-js
-                :id="state.id"
-                :type="state.type"
-                ref="chartRef"
-                :data="state.data">
+                v-bind="state"
+                ref="chartRef">
             </vue3-chart-js>
         </div>
     </div>
@@ -23,15 +21,17 @@ export default {
         Vue3ChartJs
     },
     setup () {
+        // 차트를 업데이트 하기 위해 연결
         const chartRef = ref(null);
 
         const state = reactive({
-            id: 'doughnut',
-            type: 'doughnut',
+            
+            type: 'bar',
             data: {
                 labels : [],
                 datasets : [
-                    { 
+                    {
+                        label: '시간대별 주문수량',
                         backgroundColor: [],
                         data: []
                     }
@@ -40,6 +40,7 @@ export default {
             token: sessionStorage.getItem("TOKEN")
         });
 
+        // 
         const handleLoadData = async()=> {
             const url = `/seller/grouphour`;
             const headers = {
@@ -48,6 +49,26 @@ export default {
             };
             const response = await axios.get(url, {headers});
             console.log(response.data.result);
+
+            if (response.data.status === 200) {
+                // [{_id:111, count:2}, {_id:222, count:3},]
+
+                let label = [];
+                let background = [];
+                let data = [];
+
+                for(let tmp of response.data.result){
+                    label.push(tmp._id);
+                    background.push('#41B883', '#E46651', '#00D8FF');
+                    data.push(tmp.count);
+                }
+
+                state.data.labels = label;
+                state.data.datasets[0]['backgroundColor'] = background;
+                state.data.datasets[0]['data'] = data;
+
+                chartRef.value.update(250); // 업데이트 필수
+            }
         }
 
         onMounted( ()=> {
