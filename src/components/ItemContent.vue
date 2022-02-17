686364772d481dc73d04b9d254f9819390d53d74
dlxorb96 <blesty5683@naver.com>
2022-02-17 17:45:34 +0900

    20220217

--- src/components/ItemContent.vue
+++ src/components/ItemContent.vue
@@ -3,20 +3,43 @@
         <h3>물품 상세 조회</h3>
         {{ state }}
 
-        <div v-if="state.item">
-            <img :src="state.item.imageUrl" style="width: 200px">
-            {{state.item._id}} <br />
-            {{state.item.name}} <br />
-            {{state.item.price}} <br />
-            {{state.item.content}} <br />
+        <div v-if="state.item" style="width:800px">
+            <el-descriptions title="상세 주문 페이지" :column="3" border>
+                <el-descriptions-item label="물품번호" label-align="center" align="center" width="150px">
+                    {{state.item._id}}
+                </el-descriptions-item>
+
+                <el-descriptions-item label="물품이름" label-align="center" align="center">
+                    {{state.item.name}}
+                </el-descriptions-item>
+
+                <el-descriptions-item label="가격" label-align="center" align="center">
+                    {{state.item.price}}
+                </el-descriptions-item>
+
+                <el-descriptions-item label="내용" label-align="center" align="center">
+                    {{state.item.content}}
+                </el-descriptions-item>
+
+            </el-descriptions>
+
+            <div class="demo-image">
+                <el-image
+                    style="width: 450px; height: 450px"
+                    :src="state.item.imageUrl"
+                    :fit="cover"
+                ></el-image>
+            </div>
+
             <select v-model="state.item.ordercnt">
                 <option v-for="no in 100" :key="no">
                     {{ no }}
                 </option>
             </select>
             <button @click="handleOrderAction">주문하기</button>
-            
-            
+            <button @click="handleCartAction">장바구니</button>
+            <button @click="handleSelectCartAction">장바구니 목록</button>
+
         </div>
     </div>
 </template>
@@ -34,6 +57,28 @@ export default {
         const state = reactive({
             code: route.query.code,
         });
+
+        const handleSelectCartAction = async()=> {
+            const url = `/shop/selectcart`;
+            const headers = {"Content-Type":"application/json"};
+            
+            const response = await axios.get(url, {headers});
+            console.log(response.data);
+        }
+
+        const handleCartAction = async()=> {
+            const url = `/shop/insertcart`;
+            const headers = {"Content-Type":"application/json"};
+
+            console.log();
+            // 물품번호, 수량, 로그인하지 않은 사용자의 정보
+            const body = {
+                code: state.item._id,
+                cnt: state.item.ordercnt
+            };
+            const response = await axios.post(url, body, {headers});
+            console.log(response.data);
+        };
 
         const handleOrderAction = async()=> {
             router.push({name:"Order",
@@ -60,7 +105,12 @@ export default {
             await handleLoadData();
         })
 
-        return {state, handleOrderAction}
+        return {
+            state, 
+            handleOrderAction, 
+            handleCartAction,
+            handleSelectCartAction
+        }
     }
 }
 </script>
